68061cdeeb3c9ec21c49dad6cbd9457cb1eb73e8
Rebne <renehgren@gmail.com>
2024-04-24 21:45:34 +0300

    Moved table initialization to initalizeSQLiteTables function for cleaner code

--- main.go
+++ main.go
@@ -41,85 +41,7 @@ func init() {
 		log.Fatal(err)
 	}
 
-	// Foreign keys needs to enabled in SQLite database
-	var isForeignKeysEnabled int
-	err = db.QueryRow("PRAGMA foreign_keys;").Scan(&isForeignKeysEnabled)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if isForeignKeysEnabled != 1 {
-		_, err = db.Exec("PRAGMA foreign_keys = ON;")
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	// Initializing table for users if it doesn't exist yet
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY,
-        username TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL,
-		date_created TIMESTAMP NOT NULL
-    );`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Initializing table for posts if it doesn't exist yet
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS posts (
-        id INTEGER PRIMARY KEY,
-		users_id INTEGER,
-        content TEXT NOT NULL,
-        category TEXT NOT NULL,
-		date TIMESTAMP NOT NULL,
-		FOREIGN KEY (users_id) REFERENCES users (id)
-    );`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Initializing table for comments if it doesn't exist yet
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS comments (
-        id INTEGER PRIMARY KEY,
-        posts_id INTEGER NOT NULL,
-		content TEXT NOT NULL,
-		date TIMESTAMP NOT NULL,
-		users_id INTEGER NOT NULL,
-		FOREIGN KEY (users_id) REFERENCES users (id),
-		FOREIGN KEY (posts_id) REFERENCES posts (id)
-    );`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Initializing table for comments_likes if it doesn't exist yet
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS comments_likes (
-        id INTEGER PRIMARY KEY,
-		users_id INTEGER NOT NULL,
-		comments_id INTEGER NOT NULL,
-        posts_id INTEGER NOT NULL,
-		is_dislike INTEGER NOT NULL
-		FOREIGN KEY (users_id) REFERENCES users (id),
-		FOREIGN KEY (posts_id) REFERENCES posts (id),
-		FOREIGN KEY (comments_id) REFERENCES comments (id)
-    );`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Initializing table for posts_likes if it doesn't exist yet
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS posts_likes (
-        id INTEGER PRIMARY KEY,
-		users_id INTEGER NOT NULL,
-		comments_id INTEGER NOT NULL,
-		is_dislike INTEGER NOT NULL
-		FOREIGN KEY (users_id) REFERENCES users (id),
-		FOREIGN KEY (comments_id) REFERENCES comments (id)
-    );`)
-	if err != nil {
-		log.Fatal(err)
-	}
+	initializeSQLiteTables(db)
 
 	defer db.Close()
 }
@@ -236,5 +158,89 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
+	}
+}
+
+func initializeSQLiteTables(db *sql.DB) {
+	var err error
+
+	// Foreign keys needs to enabled in SQLite database
+	var isForeignKeysEnabled int
+	err = db.QueryRow("PRAGMA foreign_keys;").Scan(&isForeignKeysEnabled)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if isForeignKeysEnabled != 1 {
+		_, err = db.Exec("PRAGMA foreign_keys = ON;")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Initializing table for users if it doesn't exist yet
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+		date_created TIMESTAMP NOT NULL
+    );`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Initializing table for posts if it doesn't exist yet
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY,
+		users_id INTEGER,
+        content TEXT NOT NULL,
+        category TEXT NOT NULL,
+		date TIMESTAMP NOT NULL,
+		FOREIGN KEY (users_id) REFERENCES users (id)
+    );`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Initializing table for comments if it doesn't exist yet
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS comments (
+        id INTEGER PRIMARY KEY,
+        posts_id INTEGER NOT NULL,
+		content TEXT NOT NULL,
+		date TIMESTAMP NOT NULL,
+		users_id INTEGER NOT NULL,
+		FOREIGN KEY (users_id) REFERENCES users (id),
+		FOREIGN KEY (posts_id) REFERENCES posts (id)
+    );`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Initializing table for comments_likes if it doesn't exist yet
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS comments_likes (
+        id INTEGER PRIMARY KEY,
+		users_id INTEGER NOT NULL,
+		comments_id INTEGER NOT NULL,
+        posts_id INTEGER NOT NULL,
+		is_dislike INTEGER NOT NULL
+		FOREIGN KEY (users_id) REFERENCES users (id),
+		FOREIGN KEY (posts_id) REFERENCES posts (id),
+		FOREIGN KEY (comments_id) REFERENCES comments (id)
+    );`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Initializing table for posts_likes if it doesn't exist yet
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS posts_likes (
+        id INTEGER PRIMARY KEY,
+		users_id INTEGER NOT NULL,
+		comments_id INTEGER NOT NULL,
+		is_dislike INTEGER NOT NULL
+		FOREIGN KEY (users_id) REFERENCES users (id),
+		FOREIGN KEY (comments_id) REFERENCES comments (id)
+    );`)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
