fd9627a5f483bf9c579a4dfe7d05c6013d062686
Karmo Kupits <karmo.kupits@gmail.com>
2024-05-09 11:20:27 +0300

    Removed incorrect cookie handling

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/gob"
 	"encoding/hex"
 	"errors"
@@ -13,16 +12,12 @@ import (
 	"strings"
 	"time"
 
-	"literary-lions/internal/cookies"
-
 	"database/sql"
 
 	// underscore is used when no package's exported identifiers (functions, types, variables) are used in your code.
 	_ "github.com/mattn/go-sqlite3"
 	"golang.org/x/crypto/bcrypt"
 )
-
-var secret []byte
 
 type User struct {
 	Name     string
@@ -60,8 +55,6 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
-	mux.HandleFunc("/set", setCookieHandler)
-	mux.HandleFunc("/get", getCookieHandler)
 	mux.HandleFunc("/", indexHandler)
 	mux.HandleFunc("/login", loginHandler)
 	mux.HandleFunc("/register", registerHandler)
@@ -354,81 +347,6 @@ func isValidEmail(email string) bool {
 	regex := regexp.MustCompile(pattern)
 
 	return regex.MatchString(email)
-}
-
-func setCookieHandler(w http.ResponseWriter, r *http.Request) {
-	// Initialize a User struct containing the data that we want to store in the
-	// cookie.
-	user := User{Name: "Alice", Password: "BadPassword"}
-
-	// Initialize a buffer to hold the gob-encoded data.
-	var buf bytes.Buffer
-
-	// Gob-encode the user data, storing the encoded output in the buffer.
-	err := gob.NewEncoder(&buf).Encode(&user)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "server error", http.StatusInternalServerError)
-		return
-	}
-
-	// Call buf.String() to get the gob-encoded value as a string and set it as
-	// the cookie value.
-	cookie := http.Cookie{
-		Name:     "exampleCookie",
-		Value:    buf.String(),
-		Path:     "/",
-		MaxAge:   3600,
-		HttpOnly: true,
-		Secure:   true,
-		SameSite: http.SameSiteLaxMode,
-	}
-
-	// Write an encrypted cookie containing the gob-encoded data as normal.
-	err = cookies.WriteEncrypted(w, cookie, secret)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "server error", http.StatusInternalServerError)
-		return
-	}
-
-	w.Write([]byte("cookie set!"))
-}
-
-func getCookieHandler(w http.ResponseWriter, r *http.Request) {
-	// Read the gob-encoded value from the encrypted cookie, handling any errors
-	// as necessary.
-	gobEncodedValue, err := cookies.ReadEncrypted(r, "exampleCookie", secret)
-	if err != nil {
-		switch {
-		case errors.Is(err, http.ErrNoCookie):
-			http.Error(w, "cookie not found", http.StatusBadRequest)
-		case errors.Is(err, cookies.ErrInvalidValue):
-			http.Error(w, "invalid cookie", http.StatusBadRequest)
-		default:
-			log.Println(err)
-			http.Error(w, "server error", http.StatusInternalServerError)
-		}
-		return
-	}
-
-	// Create a new instance of a User type.
-	var user User
-
-	// Create an strings.Reader containing the gob-encoded value.
-	reader := strings.NewReader(gobEncodedValue)
-
-	// Decode it into the User type. Notice that we need to pass a *pointer* to
-	// the Decode() target here?
-	if err := gob.NewDecoder(reader).Decode(&user); err != nil {
-		log.Println(err)
-		http.Error(w, "server error", http.StatusInternalServerError)
-		return
-	}
-
-	// Print the user information in the response.
-	fmt.Fprintf(w, "Name: %q\n", user.Name)
-	fmt.Fprintf(w, "Password: %q\n", user.Password)
 }
 
 func initializeSQLiteTables(db *sql.DB) {
